788a9936f98cbf3ce15fd95a068bd701c3a3dbb0
townsend.bishop <townsend.bishop@vashonsd.org>
2021-10-21 11:04:32 -0700

    Added code.

--- src/org/vashonsd/Main.java
+++ src/org/vashonsd/Main.java
@@ -3,14 +3,15 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-           Scanner scan = new Scanner(System.in);
-             System.out.println("give number of dollars");
-            int dollars = scan.nextInt();
-            System.out.println("give number of cents");
-            int cents = scan.nextInt();
-           System.out.println((double) dollars + (double) cents * 0.01 +"is your total");
+        Scanner scan = new Scanner(System.in);
+        System.out.println("What is the cost of the meal?");
+        double cost = scan.nextDouble();
+        System.out.println("You are paying $" + cost + " for the meal.");
+        System.out.println("Your tip at 15% is: " + cost * .15);
+        double tip = cost * .15;
+        tip = Math.round(tip * 100.0) / 100.0;
+        System.out.println("Your total cost is: " + (cost + tip));
 
-     // System.out.print( 30 / 60 );
 
 
 
